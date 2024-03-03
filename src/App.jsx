6ad0a57284e4a7cf185ab72e67bf47437904c985
creixz <s.freddy131@gmail.com>
2024-03-02 21:44:00 -0500

    code-questor-v1.1

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import './App.css'
 import Navbar from './navbar/Navbar'
+import Questions from './questions/Questions'
 
 function App() {
 
   return (
     <>
       <Navbar />
+      <div className="container">
+        <Questions />
+      </div>
     </>
   )
 }
---
+++ src/questions/Questions.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import './Questions.css'
+
+const Questions = () => {
+    return (
+        <section className="main__container">
+            <div className='main__content'>
+                <div className="button__container">
+                    <button className='question__button'></button>
+                </div>
+                <div className="wrapper">
+                    <div className="question__container">
+
+                    </div>
+                    <div className="aswer__container">
+
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Questions
